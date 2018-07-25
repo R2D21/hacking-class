8d906986a3863f16693501eddeb9570f177d1a0a
bbpucca <39225667+bbpucca@users.noreply.github.com>
2018-07-26 01:44:40 +0200

    Update 20180725201059_create_students.rb

--- hacking-class/db/migrate/20180725201059_create_students.rb
+++ hacking-class/db/migrate/20180725201059_create_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
   def change
-    create_table :students do |t|
+    create_table :students do |t|               # créer une table "étudiant"
       t.string :name
-      t.belongs_to :course, index: true
+      t.belongs_to :course, index: true             # un étudiant a un nom et un seul cours
       t.timestamps
     end
   end
