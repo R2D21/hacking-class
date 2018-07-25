60d00806f3fb9958b01c8b676d1f12084e82add6
bbpucca <39225667+bbpucca@users.noreply.github.com>
2018-07-26 00:50:00 +0200

    Update 20180725201050_create_courses.rb

--- hacking-class/db/migrate/20180725201050_create_courses.rb
+++ hacking-class/db/migrate/20180725201050_create_courses.rb
@@ -1,8 +1,8 @@
 class CreateCourses < ActiveRecord::Migration[5.2]
   def change
-    create_table :courses do |t|
+    create_table :courses do |t|           # créer une table "cours"
       t.string :title
-      t.text :content
+      t.text :content               # un cours a un titre et un contenu
       t.timestamps
     end
   end
